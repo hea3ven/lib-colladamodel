83863597ce4773854497bd0c2a71f8a3e9f6deb7
Hea3veN <hea3venmc@gmail.com>
2014-02-26 22:57:03 -0300

    Applied the GNU LGPL license.

--- src/com/hea3ven/colladamodel/client/model/collada/Transform.java
+++ src/com/hea3ven/colladamodel/client/model/collada/Transform.java
@@ -1,3 +1,24 @@
+/**
+ * 
+ * Copyright (c) 2014 Hea3veN
+ * 
+ *  This file is part of lib-colladamodel.
+ *
+ *  lib-colladamodel is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  lib-colladamodel is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with lib-colladamodel.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.hea3ven.colladamodel.client.model.collada;
 
 public abstract class Transform {
